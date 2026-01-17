3bde02184d9e5e8a44d8f8926eee1b6913bdf1e9
404-Fr4nc1s <marcinkowskifrancis917@gmail.com>
2026-01-17 12:03:52 +0100

    git initial

---
+++ test/testporte/cont/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Nom de la ressource
+name "Interieur bureau faux papier"
+description 'MLO d un bureau pour le script "faux_papier".'
+author '404-Fr4nc1s'
+version '2.0.0'
+
+this_is_a_map 'yes'
+
+-- Fichiers de la ressource
+files {
+    'stream/prop_elecbox_24.ydr',
+    'stream/contenaire.ymap',
+    'stream/door.ymap'
+}
+
+data_file 'MAP_YMAP' 'stream/contenaire.ymap'
+data_file 'MAP_YMAP' 'stream/door.ymap'
